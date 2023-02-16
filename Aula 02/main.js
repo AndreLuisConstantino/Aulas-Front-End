75aef909feb1f06651ad85ac18ccc6e9095a2458
André <andreluis081205@gmail.com>
2023-02-16 16:59:28 -0300

    object add

--- Aula 02/main.js
+++ Aula 02/main.js
@@ -7,14 +7,12 @@ const adicionar = document.getElementById('adicionar')
 //     container.innerHTML = container.innerHTML +'<div class="item">Senai</div>'
 // }
 
-const adicionarCard = (nomeDigitado, notaDigitada) => {
-    const nota = notaDigitada
-    const nomeVar = nomeDigitado
+const adicionarCard = (aluno) => {
     const container = document.getElementById('container')
     const novaDiv = document.createElement('div')
     novaDiv.classList.add('aluno')
-    novaDiv.innerHTML = `<h2 class="aluno__nome">${nomeVar}</h2>
-                        <span class="aluno__nota">${nota}</span>`
+    novaDiv.innerHTML = `<h2 class="aluno__nome">${aluno.nome}</h2>
+                        <span class="aluno__nota">${aluno.nota}</span>`
 
     if(nota > 5){
         novaDiv.classList.add('sombra-azul')
@@ -26,8 +24,25 @@ const adicionarCard = (nomeDigitado, notaDigitada) => {
 }
 
 const handleClick = () => {
-    const nome = prompt('Digite o nome do aluno:')
-    const nota = prompt('Digite uma nota:').replace(',','.')
+
+    const aluno = {
+        nome: prompt('Digite o nome do aluno'),
+        nota: prompt('Digite uma nota:').replace(',','.')
+    }
+
+    if(fazerVerfificacaoNome(aluno.nome) == true || fazerVerfificacao(aluno.nota)){
+        
+    } else {
+        alert('Algo deu errado')
+    }
+    
+    
+}
+
+const fazerVerfificacaoNome = (nomeDigitado, notaDigitada) => {
+    let nome = nomeDigitado
+    let nota = notaDigitada
+    let status = true
 
     if(nome == '' || nota == ''){
         alert('Você deixou algum dos espaços em branco.')
@@ -40,11 +55,30 @@ const handleClick = () => {
     }
     else if(nota == null || nome == null){
         alert('Você esqueceu de digitar algum valor')
+    } else {
+        return nome
     }
-    else {
-        adicionarCard(nome, nota)
+}
+
+const fazerVerfificacao = (nomeDigitado, notaDigitada) => {
+    let nome = nomeDigitado
+    let nota = notaDigitada
+    let status = true
+
+    if(nome == '' || nota == ''){
+        alert('Você deixou algum dos espaços em branco.')
+    } else if(isNaN(nota)){
+        alert('Você não digitou números.')
+    } else if(nota < 0 || nota > 10){
+        alert('Digite uma nota válida')
+    } else if(!isNaN(nome)) {
+        alert('Você digitou numeros no lugar do nome')
     }
-    
+    else if(nota == null || nome == null){
+        alert('Você esqueceu de digitar algum valor')
+    } else {
+        return nota
+    }
 }
 
 adicionar.addEventListener('click', handleClick)
